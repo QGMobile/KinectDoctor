95f65be0c1d55012474cd4dfa7f8ded9a1956f2f
TZH <zhihuangtang@gmail.com>
2016-10-27 13:57:32 +0800

    Add starter pages.

--- app/src/main/java/com/qg/kinectdoctor/activity/MainActivity.java
+++ app/src/main/java/com/qg/kinectdoctor/activity/MainActivity.java
@@ -1,25 +1,9 @@
 package com.qg.kinectdoctor.activity;
 
 
-import android.content.Context;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.qg.kinectdoctor.R;
-import com.qg.kinectdoctor.emsdk.IMManager;
-import com.qg.kinectdoctor.emsdk.LoginCallback;
-import com.qg.kinectdoctor.fragment.ChatListFragment;
-import com.qg.kinectdoctor.logic.LogicHandler;
-import com.qg.kinectdoctor.logic.LogicImpl;
-import com.qg.kinectdoctor.param.LoginParam;
-import com.qg.kinectdoctor.result.LoginResult;
-import com.qg.kinectdoctor.util.ToastUtil;
-
 
 
 public class MainActivity extends BaseActivity {
