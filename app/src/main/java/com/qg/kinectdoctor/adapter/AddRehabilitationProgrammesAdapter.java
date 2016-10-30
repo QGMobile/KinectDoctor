d68723b4f9f2b4b2c65fde4224e930def38008c3
ZhuangJinKun <1223645484@qq.com>
2016-10-30 23:49:37 +0800

    写了逻辑，完善了之前的东西

--- app/src/main/java/com/qg/kinectdoctor/adapter/AddRehabilitationProgrammesAdapter.java
+++ app/src/main/java/com/qg/kinectdoctor/adapter/AddRehabilitationProgrammesAdapter.java
@@ -8,44 +8,41 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.qg.kinectdoctor.R;
-import com.qg.kinectdoctor.model.RcStage;
+import com.qg.kinectdoctor.model.Action;
 
 import java.util.ArrayList;
 
 /**
  * Created by 攀登者 on 2016/10/30.
  */
-public class RehabilitationProgrammesAdapter extends ArrayAdapter<RcStage> {
+public class AddRehabilitationProgrammesAdapter extends ArrayAdapter<Action> {
 
     private int resourceId;
 
-    public RehabilitationProgrammesAdapter(Context context, int resource, ArrayList<RcStage> objects) {
+    public AddRehabilitationProgrammesAdapter(Context context, int resource, ArrayList<Action> objects) {
         super(context, resource, objects);
         resourceId = resource;
     }
 
     @Override
     public View getView(int position, View converView, ViewGroup parent) {
-        RcStage rcStage = getItem(position);
+        Action action = getItem(position);
         View view;
         ViewHolder viewHolder;
         if (converView == null) {
             view = LayoutInflater.from(getContext()).inflate(resourceId, null);
             viewHolder = new ViewHolder();
             viewHolder.stage_name = (TextView) view.findViewById(R.id.stage_name);
-            viewHolder.num = (TextView) view.findViewById(R.id.stage_num);
             view.setTag(viewHolder);
         } else {
             view = converView;
             viewHolder = (ViewHolder) view.getTag();
         }
-        viewHolder.num.setText("阶段" + rcStage.getNum());
-        viewHolder.stage_name.setText(rcStage.getActionName());
+        viewHolder.stage_name.setText(action.getName());
         return view;
     }
 
     class ViewHolder {
-        TextView num;
         TextView stage_name;
     }
 }
---
+++ app/src/main/java/com/qg/kinectdoctor/param/getActionsParam.java
@@ -0,0 +1,7 @@
+package com.qg.kinectdoctor.param;
+
+/**
+ * Created by 攀登者 on 2016/10/30.
+ */
+public class GetActionsParam extends Param {
+}
